23ded4220994154724776f07c80b880bcc78fa4b
Darkcoder-enginner <vikassingh@wink.cloud>
2026-09-26 00:28:22 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+   
+   var freq = new Dictionary<int, int>();
+
+    foreach (int num in nums)
+    {
+        if (!freq.ContainsKey(num))
+            freq[num] = 0;
+
+        freq[num]++;
+    }
+
+    return freq
+        .OrderByDescending(x => x.Value)
+        .Take(k)
+        .Select(x => x.Key)
+        .ToArray();  
+          
+    }
+}
